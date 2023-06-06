c259eae460e31c6a5ecda77d00aec590d9144223
okera johnson <okerajohnson@gmail.com>
2023-06-06 14:00:05 -0400

    lighter narrate version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,151 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState } from "react";
+import styles from "./index.module.css";
+
+//set input states for form
+export default function App() {
+  const [nameInput, setNameInput] = useState("");
+  const [prodservInput, setProdservInput] = useState("");
+  const [visionInput, setVisionInput] = useState("");
+  const [missionInput, setMissionInput] = useState("");
+  const [problemInput, setProblemInput] = useState("");
+  const [solutionInput, setSolutionInput] = useState("");
+  const [futureInput, setFutureInput] = useState("");
+  const [colorInput, setColorInput] = useState("");
+  const [villianInput, setVillianInput] = useState("");
+  const [heroInput, setHeroInput] = useState("");
+  const [descripInput, setDescripInput] = useState("");
+  const [result, setResult] = useState();
+
+  //submit data to generate function
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name: nameInput, prodserv: prodservInput, vision: visionInput, mission: missionInput, problem: problemInput, solution: solutionInput,
+        future: futureInput, colors: colorInput, villian: villianInput, hero: heroInput, descrip: descripInput }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setResult(data.result);
+      setNameInput("");
+      setProdservInput("");
+      setVisionInput("");
+      setMissionInput("");
+      setProblemInput("");
+      setSolutionInput("");
+      setFutureInput("");
+      setColorInput("");
+      setVillianInput("");
+      setHeroInput("");
+      setDescripInput("");
+    } catch(error) {
+      // error handling logic here
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+        <title>Business Narrative Generator</title>
+        
+      
+
+      <main className={styles.main}>
+        <img src="/logo192.png" className={styles.icon} />
+        <h3>Create your narrative</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter an company name"
+            value={nameInput}
+            onChange={(e) => setNameInput(e.target.value)}
+          />
+          <input
+            type="text"
+            name="prodserv"
+            placeholder="Enter your product or service"
+            value={prodservInput}
+            onChange={(e) => setProdservInput(e.target.value)}
+          />
+          <input
+            type="text"
+            name="vision"
+            placeholder="Enter the vision of the organization"
+            value={visionInput}
+            onChange={(e) => setVisionInput(e.target.value)}
+          />
+          <input
+            type="text"
+            name="mission"
+            placeholder="Enter the mission of the organization"
+            value={missionInput}
+            onChange={(e) => setMissionInput(e.target.value)}
+          />
+          <input
+            type="text"
+            name="problem"
+            placeholder="What problem are you solving"
+            value={problemInput}
+            onChange={(e) => setProblemInput(e.target.value)}
+          />
+          <input
+            type="text"
+            name="solution"
+            placeholder="What is the solution to the problem"
+            value={solutionInput}
+            onChange={(e) => setSolutionInput(e.target.value)}
+          />
+          <input
+            type="text"
+            name="future"
+            placeholder="what is the future you are fighting for"
+            value={futureInput}
+            onChange={(e) => setFutureInput(e.target.value)}
+          />
+          <input
+            type="text"
+            name="descrip"
+            placeholder="Enter descriptive words that best describe your organization"
+            value={descripInput}
+            onChange={(e) => setDescripInput(e.target.value)}
+          />
+          <input
+            type="text"
+            name="colors"
+            placeholder="What colors represent your organization"
+            value={colorInput}
+            onChange={(e) => setColorInput(e.target.value)}
+          />
+          <input
+            type="text"
+            name="villian"
+            placeholder="What is the villian of your organization"
+            value={villianInput}
+            onChange={(e) => setVillianInput(e.target.value)}
+          />
+          <input
+            type="text"
+            name="hero"
+            placeholder="If your organization had a hero who would it be"
+            value={heroInput}
+            onChange={(e) => setHeroInput(e.target.value)}
+          />
+          <input type="submit" value="Generate narrative" />
+        </form>
+        <div className={styles.result}>{result}</div>
+      </main>
     </div>
   );
 }
-
-export default App;
---
+++ src/api/generate.js
@@ -0,0 +1,153 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please follow instructions in README.md",
+      }
+    });
+    return;
+  }
+
+  const name = req.body.name || '';
+  if (name.length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid name",
+      }
+    });
+    return;
+  }
+
+  const prodserv = req.body.prodserv || '';
+  if (prodserv.length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid prodserv",
+      }
+    });
+    return;
+  }
+  const mission = req.body.mission || '';
+  if (mission.length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid mission",
+      }
+    });
+    return;
+  }
+
+  const vision = req.body.vision || '';
+  if (vision.length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid vision",
+      }
+    });
+    return;
+  }
+  const problem = req.body.problem || '';
+  if (problem.length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid problem",
+      }
+    });
+    return;
+  }
+  const solution = req.body.solution || '';
+  if (solution.length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid solution",
+      }
+    });
+    return;
+  }
+  const future = req.body.future || '';
+  if (future.length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid future",
+      }
+    });
+    return;
+  }
+  const descrip = req.body.descrip || '';
+  if (descrip.length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid descrip",
+      }
+    });
+    return;
+  }
+  const colors = req.body.colors || '';
+  if (colors.length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid colors",
+      }
+    });
+    return;
+  }
+  const villian = req.body.villian || '';
+  if (villian.length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid villian",
+      }
+    });
+    return;
+  }
+  const hero = req.body.hero || '';
+  if (hero.length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid hero",
+      }
+    });
+    return;
+  }
+
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(name, prodserv, mission, problem, descrip, hero, colors, villian, vision, future, solution ),
+      temperature: 1,
+      max_tokens: 100,
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
+  } catch(error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      });
+    }
+  }
+}
+
+function generatePrompt(name, prodserv, mission, vision, hero, villian, problem, solution, descrip, colors, future) {
+  
+    return `You are a brand strategist write a creative narrative based on the data provided
+
+
+
+Business: The organization is called ${name} they offer ${prodserv} their goal is ${mission} and they beleive in ${vision}. The organization believes ${problem} is an issue and can be solved with ${solution}. They fight for ${future} changes in the world. ${descrip} are the words and ${colors} are the colors that best describe this organization. The organization beleives ${villian} is the enemy they are against and ${hero} is a hero on their side.
+Response:`;
+}
